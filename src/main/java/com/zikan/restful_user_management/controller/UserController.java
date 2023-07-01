7fe467418e7c8a56a3c1fe5e60e03246eafcc6bd
Isaac Sunday Arinze <sunnyanas6@gmail.com>
2023-07-01 19:18:34 +0100

    swaager configuration added

--- src/main/java/com/zikan/restful_user_management/controller/UserController.java
+++ src/main/java/com/zikan/restful_user_management/controller/UserController.java
@@ -5,6 +5,9 @@ import com.zikan.restful_user_management.entity.User;
 import com.zikan.restful_user_management.exception.ErrorDetails;
 import com.zikan.restful_user_management.exception.ResourceNotFoundException;
 import com.zikan.restful_user_management.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,6 +19,10 @@ import org.springframework.web.context.request.WebRequest;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Tag(
+        name = "CRUD REST APIs for User Resource",
+        description = "CRUD REST APIs - Create User, Update User, Get User, Delete User, Delete All Users"
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api")
@@ -23,11 +30,29 @@ public class UserController {
 
     private UserService userService;
 
+    @Operation(
+            summary = "Create User REST API",
+            description = "Create User REST API is used to save user details in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP status 201 created"
+    )
+
     @PostMapping("/create")
     public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user) {
         UserDto savedUser = userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
+
+    @Operation(
+            summary = "Get User By Id REST API",
+            description = "Get a single user from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP status 200 Success!"
+    )
 
 //    Build Get REST API by ID
     @GetMapping("{id}")
@@ -36,11 +61,29 @@ public class UserController {
         UserDto user = userService.getUserById(userId);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
+
+    @Operation(
+            summary = "Get Users REST API",
+            description = " Get All User from the  database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP status 200 Successful"
+    )
     @GetMapping
     public ResponseEntity <List<UserDto>> getAllUsers(){
         List<UserDto> users = userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
+
+    @Operation(
+            summary = "Update User REST API",
+            description = "Update User REST API is used to update a particular user in the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP status 201 success"
+    )
 //    build update user REST API
     @PutMapping("/{id}")
     public ResponseEntity<UserDto> updateUser (@PathVariable ("id") Long userId, @RequestBody @Valid UserDto user){
@@ -48,6 +91,15 @@ public class UserController {
         UserDto updatedUser = userService.updateUser(user);
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
+
+    @Operation(
+            summary = "Delete User REST API",
+            description = "Delete User REST API is used to delete user from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP status 200 Success"
+    )
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteUser(@PathVariable ("id") Long userId){
         userService.deleteUser(userId);
--- src/main/java/com/zikan/restful_user_management/dto/UserDto.java
+++ src/main/java/com/zikan/restful_user_management/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.zikan.restful_user_management.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+@Schema(
+        description = "UserDto Model Information"
+)
 @Setter
 @Getter
 @AllArgsConstructor
@@ -14,10 +19,22 @@ import lombok.Setter;
 public class UserDto {
     private Long id;
 
+    @Schema(
+            description = "User First Name"
+    )
+
     @NotEmpty (message = "please input your firstName")
     private String firstName;
+
+    @Schema(
+            description = "User last Name"
+    )
     @NotEmpty (message = "kindly add firstname")
     private String lastName;
+
+    @Schema(
+            description = "User Email Address"
+    )
     @NotEmpty (message = "Please provide a valid email address")
     @Email
     private String email;
